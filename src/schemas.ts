e82eff1fbc5c6e6ce09c4523371e5c5da6b9dddf
Menard Maranan <menardmaranan.codes@gmail.com>
2024-10-11 04:14:08 +0800

    added constraints to form upload schema check

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const FileUploadSchema = z.object({
-    passphrase: z.string(),
+    passphrase: z.string().min(4, 'Minimum length of passphrase is at least 4').max(40, 'Max passphrase reached'),
     expiration: z.enum(['7 Days', '3 Days', '24 Hours', '12 Hours', '4 Hours', '1 Hour', '5 Minutes'])
 });
 
